0a36cd6eb3d77c459186fca38f4cdafeea33b1ad
skai273 <kinoshita116@gmail.com>
2025-05-30 20:51:11 +0900

    implement beginner contest 157 a

---
+++ src/bin/beginner_contest_157/a.rs
@@ -0,0 +1,8 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: u32 = input.trim().parse().unwrap();
+    println!("{}", (n + 2 - 1) / 2)
+}
