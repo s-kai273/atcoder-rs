c94a171590a26b43f2c34abfc0a1e2468ea9faa8
skai273 <kinoshita116@gmail.com>
2025-06-05 01:19:07 +0900

    improve beginner contest 403 b

--- src/bin/beginner_contest_403/ b.rs
+++ src/bin/beginner_contest_403/ b.rs
@@ -1,25 +1,19 @@
 use std::io::BufRead;
 
 fn compare_char(t: char, u: char) -> bool {
-    if t == '?' || t == u { true } else { false }
+    t == '?' || t == u
 }
 
 fn main() {
     let mut lines = std::io::stdin().lock().lines();
     let t_list: Vec<char> = lines.next().unwrap().unwrap().chars().collect();
     let u_list: Vec<char> = lines.next().unwrap().unwrap().chars().collect();
-    let mut answer = "No";
-
-    for i in 0..t_list.len() - u_list.len() + 1 {
-        for j in 0..u_list.len() {
-            if !compare_char(t_list[i + j], u_list[j]) {
-                break;
-            }
-            if j == u_list.len() - 1 {
-                answer = "Yes";
-            }
-        }
-    }
-
+    let result = t_list.windows(u_list.len()).any(|window| {
+        window
+            .iter()
+            .zip(&u_list)
+            .all(|(&t, &u)| compare_char(t, u))
+    });
+    let answer = if result { "Yes" } else { "No" };
     println!("{}", answer);
 }
