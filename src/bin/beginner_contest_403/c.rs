23f5aae369004e0595da67af44133ded181f1f45
skai273 <kinoshita116@gmail.com>
2025-06-06 01:03:55 +0900

    improve beginner contest 403 c

--- src/bin/beginner_contest_403/c.rs
+++ src/bin/beginner_contest_403/c.rs
@@ -1,40 +1,62 @@
 use std::{io::BufRead, str::SplitWhitespace, vec};
 
+#[derive(Clone)]
+enum AuthType {
+    All,
+    Pages(Vec<u32>),
+    None,
+}
+
 fn main() {
     let mut lines = std::io::stdin().lock().lines();
     let line = lines.next().unwrap().unwrap();
     let mut iter: SplitWhitespace = line.split_whitespace();
     let n: usize = iter.next().unwrap().parse().unwrap();
-    let m: usize = iter.next().unwrap().parse().unwrap();
+    let _: usize = iter.next().unwrap().parse().unwrap();
     let q: u32 = iter.next().unwrap().parse().unwrap();
-    let mut auth_list: Vec<Vec<bool>> = vec![vec![false; m]; n];
-    let query_list: Vec<Vec<usize>> = (0..q)
-        .map(|_| {
-            lines
-                .next()
-                .unwrap()
-                .unwrap()
-                .split_whitespace()
-                .map(|s| s.parse().unwrap())
-                .collect()
-        })
-        .collect();
+    let mut auth_list: Vec<AuthType> = vec![AuthType::None; n];
+    let mut answer_list: Vec<String> = Vec::new();
 
-    query_list.iter().for_each(|query| match query[0] {
-        1 => {
-            auth_list[query[1] - 1][query[2] - 1] = true;
+    (0..q).into_iter().for_each(|_| {
+        let query = lines.next().unwrap().unwrap();
+        let query: Vec<usize> = query
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+        match query[0] {
+            1 => match auth_list[query[1] - 1] {
+                AuthType::Pages(ref mut pages) => {
+                    if !pages.contains(&(query[2] as u32)) {
+                        pages.push(query[2] as u32);
+                    }
+                }
+                AuthType::All => {}
+                AuthType::None => {
+                    auth_list[query[1] - 1] = AuthType::Pages(vec![query[2] as u32]);
+                }
+            },
+            2 => {
+                auth_list[query[1] - 1] = AuthType::All;
+            }
+            3 => {
+                let answer = match &auth_list[query[1] - 1] {
+                    AuthType::All => "Yes",
+                    AuthType::Pages(pages) => {
+                        if pages.contains(&(query[2] as u32)) {
+                            "Yes"
+                        } else {
+                            "No"
+                        }
+                    }
+                    AuthType::None => "No",
+                };
+                answer_list.push(answer.to_string());
+            }
+            _ => unreachable!("invalid query"),
         }
-        2 => {
-            auth_list[query[1] - 1] = vec![true; m];
-        }
-        3 => {
-            let answer = if auth_list[query[1] - 1][query[2] - 1] {
-                "Yes"
-            } else {
-                "No"
-            };
-            println!("{}", answer);
-        }
-        _ => unreachable!("invalid query"),
+    });
+
+    answer_list.iter().for_each(|answer| {
+        println!("{}", answer);
     });
 }
