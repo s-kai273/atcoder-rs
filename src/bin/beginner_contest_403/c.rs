8784b7ff1ecee04764d8bc300194736ee1e2d592
skai273 <kinoshita116@gmail.com>
2025-06-08 00:07:42 +0900

    improve beginner contest 403 c

--- src/bin/beginner_contest_403/c.rs
+++ src/bin/beginner_contest_403/c.rs
@@ -1,4 +1,11 @@
-use std::{io::BufRead, str::SplitWhitespace, vec};
+use std::{collections::HashMap, io::BufRead, str::SplitWhitespace, vec};
+
+#[derive(Clone)]
+enum AuthType {
+    Page(HashMap<usize, u128>),
+    All,
+    None,
+}
 
 fn main() {
     let mut lines = std::io::stdin().lock().lines();
@@ -8,8 +15,7 @@ fn main() {
     let _: usize = iter.next().unwrap().parse().unwrap();
     let q: usize = iter.next().unwrap().parse().unwrap();
     let word_size = 128;
-    let mut auth_list: Vec<Vec<u128>> =
-        vec![vec![0u128; ((q + word_size - 1) / word_size) as usize]; n];
+    let mut auth_list: Vec<AuthType> = vec![AuthType::None; n];
     let mut answer_list: Vec<String> = Vec::new();
 
     (0..q).into_iter().for_each(|_| {
@@ -19,21 +25,51 @@ fn main() {
             .map(|s| s.parse().unwrap())
             .collect();
         match query[0] {
-            1 => {
-                auth_list[query[1] - 1][(query[2] + word_size - 1) / word_size - 1] |=
-                    1u128 << (query[2] % word_size);
-            }
+            1 => match &mut auth_list[query[1] - 1] {
+                AuthType::None => {
+                    let mut auth_pages = HashMap::new();
+                    auth_pages.insert(
+                        (query[2] + word_size - 1) / word_size - 1,
+                        1u128 << (query[2] % word_size),
+                    );
+                    auth_list[query[1] - 1] = AuthType::Page(auth_pages);
+                }
+                AuthType::Page(auth_pages) => {
+                    match auth_pages.get_mut(&((query[2] + word_size - 1) / word_size - 1)) {
+                        Some(page) => {
+                            *page |= 1u128 << (query[2] % word_size);
+                        }
+                        None => {
+                            auth_pages.insert(
+                                (query[2] + word_size - 1) / word_size - 1,
+                                1u128 << (query[2] % word_size),
+                            );
+                        }
+                    }
+                }
+                AuthType::All => {}
+            },
             2 => {
-                auth_list[query[1] - 1] = vec![!0u128; ((q + word_size - 1) / word_size) as usize];
+                auth_list[query[1] - 1] = AuthType::All;
             }
             3 => {
-                let answer = if auth_list[query[1] - 1][(query[2] + word_size - 1) / word_size - 1]
-                    & 1u128 << (query[2] % word_size)
-                    != 0
-                {
-                    "Yes"
-                } else {
-                    "No"
+                let answer = match &auth_list[query[1] - 1] {
+                    AuthType::None => "No",
+                    AuthType::Page(auth_pages) => {
+                        let page_opt =
+                            auth_pages.get(&((query[2] + word_size - 1) / word_size - 1));
+                        match page_opt {
+                            Some(page) => {
+                                if *page & 1u128 << (query[2] % word_size) != 0 {
+                                    "Yes"
+                                } else {
+                                    "No"
+                                }
+                            }
+                            None => "No",
+                        }
+                    }
+                    AuthType::All => "Yes",
                 };
                 answer_list.push(answer.to_string());
             }
