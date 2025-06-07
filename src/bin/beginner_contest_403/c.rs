ac60b7ddff8979a7b27eb0a1079ef16629b1978e
skai273 <kinoshita116@gmail.com>
2025-06-07 23:17:12 +0900

    improve beginner contest 403 c

--- src/bin/beginner_contest_403/c.rs
+++ src/bin/beginner_contest_403/c.rs
@@ -1,20 +1,15 @@
 use std::{io::BufRead, str::SplitWhitespace, vec};
 
-#[derive(Clone)]
-enum AuthType {
-    All,
-    Pages(Vec<u32>),
-    None,
-}
-
 fn main() {
     let mut lines = std::io::stdin().lock().lines();
     let line = lines.next().unwrap().unwrap();
     let mut iter: SplitWhitespace = line.split_whitespace();
     let n: usize = iter.next().unwrap().parse().unwrap();
     let _: usize = iter.next().unwrap().parse().unwrap();
-    let q: u32 = iter.next().unwrap().parse().unwrap();
-    let mut auth_list: Vec<AuthType> = vec![AuthType::None; n];
+    let q: usize = iter.next().unwrap().parse().unwrap();
+    let word_size = 128;
+    let mut auth_list: Vec<Vec<u128>> =
+        vec![vec![0u128; ((q + word_size - 1) / word_size) as usize]; n];
     let mut answer_list: Vec<String> = Vec::new();
 
     (0..q).into_iter().for_each(|_| {
@@ -24,31 +19,21 @@ fn main() {
             .map(|s| s.parse().unwrap())
             .collect();
         match query[0] {
-            1 => match auth_list[query[1] - 1] {
-                AuthType::Pages(ref mut pages) => {
-                    if !pages.contains(&(query[2] as u32)) {
-                        pages.push(query[2] as u32);
-                    }
-                }
-                AuthType::All => {}
-                AuthType::None => {
-                    auth_list[query[1] - 1] = AuthType::Pages(vec![query[2] as u32]);
-                }
-            },
+            1 => {
+                auth_list[query[1] - 1][(query[2] + word_size - 1) / word_size - 1] |=
+                    1u128 << (query[2] % word_size);
+            }
             2 => {
-                auth_list[query[1] - 1] = AuthType::All;
+                auth_list[query[1] - 1] = vec![!0u128; ((q + word_size - 1) / word_size) as usize];
             }
             3 => {
-                let answer = match &auth_list[query[1] - 1] {
-                    AuthType::All => "Yes",
-                    AuthType::Pages(pages) => {
-                        if pages.contains(&(query[2] as u32)) {
-                            "Yes"
-                        } else {
-                            "No"
-                        }
-                    }
-                    AuthType::None => "No",
+                let answer = if auth_list[query[1] - 1][(query[2] + word_size - 1) / word_size - 1]
+                    & 1u128 << (query[2] % word_size)
+                    != 0
+                {
+                    "Yes"
+                } else {
+                    "No"
                 };
                 answer_list.push(answer.to_string());
             }
