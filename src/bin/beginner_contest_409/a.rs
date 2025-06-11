b389862d2e3762f10b566f2cd94e2697af2668fc
skai273 <kinoshita116@gmail.com>
2025-06-11 23:14:25 +0900

    improve beginner contest 409 a

--- src/bin/beginner_contest_409/a.rs
+++ src/bin/beginner_contest_409/a.rs
@@ -5,11 +5,10 @@ fn main() {
     let n: usize = lines.next().unwrap().unwrap().parse().unwrap();
     let t_list: Vec<char> = lines.next().unwrap().unwrap().chars().collect();
     let a_list: Vec<char> = lines.next().unwrap().unwrap().chars().collect();
-    let mut answer = "No";
-    (0..n).for_each(|i| {
-        if t_list[i] == 'o' && a_list[i] == 'o' {
-            answer = "Yes";
-        }
-    });
+    let answer = if (0..n).any(|i| t_list[i] == 'o' && a_list[i] == 'o') {
+        "Yes"
+    } else {
+        "No"
+    };
     println!("{}", answer);
 }
