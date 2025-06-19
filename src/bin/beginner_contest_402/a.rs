66934fcf54888c53cd02904906521aab53ca3af3
skai273 <kinoshita116@gmail.com>
2025-06-19 21:47:39 +0900

    improve beginner contest 402 a

--- src/bin/beginner_contest_402/a.rs
+++ src/bin/beginner_contest_402/a.rs
@@ -1,11 +1,6 @@
 fn main() {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).unwrap();
-    let mut answer: Vec<char> = Vec::new();
-    for ch in s.chars() {
-        if ch.is_uppercase() {
-            answer.push(ch);
-        }
-    }
-    println!("{}", answer.into_iter().collect::<String>());
+    let answer: String = s.chars().filter(|ch| ch.is_uppercase()).collect();
+    println!("{}", answer);
 }
