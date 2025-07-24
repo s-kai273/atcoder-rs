61f2ab92bb472c611dfd68bc8d075d352ff67642
skai273 <kinoshita116@gmail.com>
2025-07-24 10:48:22 +0900

    improve beginner contest 415 b

--- src/bin/beginner_contest_415/b.rs
+++ src/bin/beginner_contest_415/b.rs
@@ -3,12 +3,12 @@ use std::io::BufRead;
 fn main() {
     let mut lines = std::io::stdin().lock().lines();
     let s: String = lines.next().unwrap().unwrap();
-    let s_map: Vec<usize> = s
+    let ch_filtered: Vec<usize> = s
         .chars()
         .enumerate()
         .filter_map(|(i, ch)| if ch == '#' { Some(i + 1) } else { None })
         .collect();
-    s_map.chunks(2).for_each(|chunks| {
+    ch_filtered.chunks(2).for_each(|chunks| {
         println!("{},{}", chunks[0], chunks[1]);
     });
 }
