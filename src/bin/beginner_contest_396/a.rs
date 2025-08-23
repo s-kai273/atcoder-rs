036bd1e0f1e967926fc18ef440fe6fbec47bfcd1
skai273 <kinoshita116@gmail.com>
2025-08-23 23:30:41 +0900

    implement beginner contest 396 a

---
+++ src/bin/beginner_contest_396/a.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut lines = std::io::stdin().lines();
+    let n: usize = lines.next().unwrap().unwrap().parse().unwrap();
+    let a_list: Vec<u32> = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split_whitespace()
+        .map(|a| a.parse().unwrap())
+        .collect();
+    let mut answer: &str = "No";
+    for i in 2..n {
+        if a_list[i - 2] == a_list[i - 1] && a_list[i - 1] == a_list[i] {
+            answer = "Yes";
+            break;
+        }
+    }
+    println!("{}", answer);
+}
