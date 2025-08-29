efa9f616ef3f7b7c5b2cb7f4de005cba011a8b1e
skai273 <kinoshita116@gmail.com>
2025-08-29 20:01:58 +0900

    implement beginner contest 420 c

--- src/bin/beginner_contest_420/c.rs
+++ src/bin/beginner_contest_420/c.rs
@@ -1,4 +1,4 @@
-use std::{cmp, collections::HashMap};
+use std::cmp;
 
 fn main() {
     let mut lines = std::io::stdin().lines();
@@ -6,54 +6,47 @@ fn main() {
     let mut iter = line.split_whitespace();
     let n: usize = iter.next().unwrap().parse().unwrap();
     let q: usize = iter.next().unwrap().parse().unwrap();
-    let a_list: Vec<u32> = lines
+    let mut a_list: Vec<u32> = lines
         .next()
         .unwrap()
         .unwrap()
         .split_whitespace()
         .map(|a| a.parse().unwrap())
         .collect();
-    let b_list: Vec<u32> = lines
+    let mut b_list: Vec<u32> = lines
         .next()
         .unwrap()
         .unwrap()
         .split_whitespace()
         .map(|b| b.parse().unwrap())
         .collect();
-    let mut a_change_map: HashMap<usize, u32> = HashMap::new();
-    let mut b_change_map: HashMap<usize, u32> = HashMap::new();
-    (0..q).for_each(|_| {
-        let line = lines.next().unwrap().unwrap();
-        let query_parts: Vec<&str> = line.split_whitespace().collect();
+    let mut min_list: Vec<u32> = (0..n).map(|i| cmp::min(a_list[i], b_list[i])).collect();
+    let mut min_sum: u64 = min_list.iter().map(|&value| value as u64).sum();
+    let query_list: Vec<String> = (0..q)
+        .map(|_| lines.next().unwrap().unwrap().trim().to_string())
+        .collect();
+    query_list.iter().for_each(|query| {
+        let query_parts: Vec<&str> = query.split_whitespace().collect();
         match query_parts[0] {
             "A" => {
                 let x: usize = query_parts[1].parse().unwrap();
                 let v: u32 = query_parts[2].parse().unwrap();
-                a_change_map.insert(x, v);
+                a_list[x - 1] = v;
+                let new_min: u32 = cmp::min(a_list[x - 1], b_list[x - 1]);
+                min_sum = min_sum - min_list[x - 1] as u64 + new_min as u64;
+                min_list[x - 1] = new_min;
+                println!("{}", min_sum);
             }
             "B" => {
                 let x: usize = query_parts[1].parse().unwrap();
                 let v: u32 = query_parts[2].parse().unwrap();
-                b_change_map.insert(x, v);
+                b_list[x - 1] = v;
+                let new_min: u32 = cmp::min(a_list[x - 1], b_list[x - 1]);
+                min_sum = min_sum - min_list[x - 1] as u64 + new_min as u64;
+                min_list[x - 1] = new_min;
+                println!("{}", min_sum);
             }
             _ => unreachable!("invalid query"),
         }
     });
-    let answer: u32 = (0..n)
-        .map(|i| {
-            let a: u32 = if let Some(&value) = a_change_map.get(&i) {
-                value
-            } else {
-                a_list[i]
-            };
-            let b: u32 = if let Some(&value) = b_change_map.get(&i) {
-                value
-            } else {
-                b_list[i]
-            };
-            cmp::min(a, b)
-        })
-        .sum();
-    println!("{}", answer);
 }
-
