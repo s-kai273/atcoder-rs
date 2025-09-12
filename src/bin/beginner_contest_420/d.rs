21f62434bd05f45d3a924e84212473b80f227acd
skai273 <kinoshita116@gmail.com>
2025-09-13 00:33:26 +0900

    implement beginner contest 420 d

--- src/bin/beginner_contest_420/d.rs
+++ src/bin/beginner_contest_420/d.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 
 fn main() {
     let mut lines = std::io::stdin().lines();
@@ -15,8 +15,7 @@ fn main() {
         for j in 0..w {
             if a_list[i][j] == 'S' {
                 s = (Some(i), Some(j));
-            }
-            if a_list[i][j] == 'G' {
+            } else if a_list[i][j] == 'G' {
                 g = (Some(i), Some(j));
             }
         }
@@ -24,76 +23,106 @@ fn main() {
 
     let s: (usize, usize) = (s.0.unwrap(), s.1.unwrap());
     let g: (usize, usize) = (g.0.unwrap(), g.1.unwrap());
-    let mut door_switched: bool = false;
-    let mut bfs_queue: VecDeque<(usize, usize)> = VecDeque::from([s]);
-    let mut count_list: Vec<Vec<i32>> = vec![vec![-1; w]; h];
-    let mut count: i32 = 0;
+    let mut bfs_queue: VecDeque<(bool, usize, usize)> = VecDeque::from([(false, s.0, s.1)]);
+    let mut count_list: HashMap<bool, Vec<Vec<i32>>> =
+        HashMap::from([(true, vec![vec![-1; w]; h]), (false, vec![vec![-1; w]; h])]);
 
-    loop {
-        let pos: (usize, usize) = bfs_queue.pop_front().unwrap();
-        match a_list[pos.0][pos.1] {
-            '.' => {
-                if count_list[pos.0][pos.1] == -1 {
-                    count_list[pos.0][pos.1] = count;
-                }
-            }
-            'S' => {
-                if count_list[pos.0][pos.1] == -1 {
-                    count_list[pos.0][pos.1] = count;
-                }
-            }
-            'G' => {
-                if count_list[pos.0][pos.1] == -1 {
-                    count_list[pos.0][pos.1] = count;
-                }
-                break;
-            }
-            'o' => {
-                if !door_switched && count_list[pos.0][pos.1] == -1 {
-                    count_list[pos.0][pos.1] = count;
-                } else {
-                    continue;
-                }
-            }
-            'x' => {
-                if door_switched && count_list[pos.0][pos.1] == -1 {
-                    count_list[pos.0][pos.1] = count;
-                } else {
-                    continue;
-                }
-            }
-            '?' => {
-                if count_list[pos.0][pos.1] == -1 {
-                    count_list[pos.0][pos.1] = count;
-                }
-                door_switched = !door_switched;
-            }
-            _ => unreachable!("Invalid charactor"),
-        }
+    count_list.get_mut(&false).unwrap()[s.0][s.1] = 0;
+    while bfs_queue.len() > 0 {
+        let pos: (bool, usize, usize) = bfs_queue.pop_front().unwrap();
+        let door_switched = if a_list[pos.1][pos.2] == '?' {
+            true
+        } else {
+            false
+        };
         for next_pos in [
-            (pos.0 + 1, pos.1),
-            (pos.0 - 1, pos.1),
-            (pos.0, pos.1 + 1),
-            (pos.0, pos.1 - 1),
+            (
+                pos.0 ^ door_switched,
+                (pos.1 as isize) + 1,
+                (pos.2 as isize),
+            ),
+            (
+                pos.0 ^ door_switched,
+                (pos.1 as isize) - 1,
+                (pos.2 as isize),
+            ),
+            (
+                pos.0 ^ door_switched,
+                (pos.1 as isize),
+                (pos.2 as isize) + 1,
+            ),
+            (
+                pos.0 ^ door_switched,
+                (pos.1 as isize),
+                (pos.2 as isize) - 1,
+            ),
         ] {
-            let ch: char = a_list[next_pos.0 as usize][next_pos.1 as usize];
+            if next_pos.1 < 0
+                || next_pos.1 >= h as isize
+                || next_pos.2 < 0
+                || next_pos.2 >= w as isize
+            {
+                continue;
+            }
+            let next_pos: (bool, usize, usize) =
+                (next_pos.0, next_pos.1 as usize, next_pos.2 as usize);
+            if count_list.get(&next_pos.0).unwrap()[next_pos.1][next_pos.2] != -1 {
+                continue;
+            }
+            let ch: char = a_list[next_pos.1 as usize][next_pos.2 as usize];
             match ch {
+                '.' => {
+                    if count_list.get(&next_pos.0).unwrap()[next_pos.1][next_pos.2] == -1 {
+                        count_list.get_mut(&next_pos.0).unwrap()[next_pos.1][next_pos.2] =
+                            count_list.get(&pos.0).unwrap()[pos.1][pos.2] + 1;
+                    }
+                }
                 '#' => continue,
+                'S' => {
+                    if count_list.get(&next_pos.0).unwrap()[next_pos.1][next_pos.2] == -1 {
+                        count_list.get_mut(&next_pos.0).unwrap()[next_pos.1][next_pos.2] =
+                            count_list.get(&pos.0).unwrap()[pos.1][pos.2] + 1;
+                    }
+                }
+                'G' => {
+                    if count_list.get(&next_pos.0).unwrap()[next_pos.1][next_pos.2] == -1 {
+                        count_list.get_mut(&true).unwrap()[next_pos.1][next_pos.2] =
+                            count_list.get(&pos.0).unwrap()[pos.1][pos.2] + 1;
+                        count_list.get_mut(&false).unwrap()[next_pos.1][next_pos.2] =
+                            count_list.get(&pos.0).unwrap()[pos.1][pos.2] + 1;
+                    }
+                    break;
+                }
                 'o' => {
-                    if door_switched {
+                    if !next_pos.0
+                        && count_list.get(&next_pos.0).unwrap()[next_pos.1][next_pos.2] == -1
+                    {
+                        count_list.get_mut(&next_pos.0).unwrap()[next_pos.1][next_pos.2] =
+                            count_list.get(&pos.0).unwrap()[pos.1][pos.2] + 1;
+                    } else {
                         continue;
                     }
                 }
                 'x' => {
-                    if !door_switched {
+                    if next_pos.0
+                        && count_list.get(&next_pos.0).unwrap()[next_pos.1][next_pos.2] == -1
+                    {
+                        count_list.get_mut(&next_pos.0).unwrap()[next_pos.1][next_pos.2] =
+                            count_list.get(&pos.0).unwrap()[pos.1][pos.2] + 1;
+                    } else {
                         continue;
+                    }
+                }
+                '?' => {
+                    if count_list.get(&next_pos.0).unwrap()[next_pos.1][next_pos.2] == -1 {
+                        count_list.get_mut(&next_pos.0).unwrap()[next_pos.1][next_pos.2] =
+                            count_list.get(&pos.0).unwrap()[pos.1][pos.2] + 1;
                     }
                 }
                 _ => {}
             }
             bfs_queue.push_back(next_pos);
         }
-        count += 1;
     }
-    println!("{}", count_list[g.0][g.1]);
+    println!("{}", count_list.get(&true).unwrap()[g.0][g.1]);
 }
