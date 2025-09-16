22eff61fa5025a1ab02c1b12f17245207805ff0b
skai273 <kinoshita116@gmail.com>
2025-09-16 23:29:05 +0900

    refactor beginner contest 422 a

--- src/bin/beginner_contest_422/a.rs
+++ src/bin/beginner_contest_422/a.rs
@@ -2,9 +2,9 @@ fn main() {
     let mut lines = std::io::stdin().lines();
     let s: String = lines.next().unwrap().unwrap().trim().to_string();
     let s_parts: Vec<u32> = s.split('-').map(|num| num.parse().unwrap()).collect();
-    let mut iter = s_parts.iter();
-    let world: u32 = *iter.next().unwrap();
-    let stage: u32 = *iter.next().unwrap();
+    let mut iter = s_parts.into_iter();
+    let world: u32 = iter.next().unwrap();
+    let stage: u32 = iter.next().unwrap();
     let next_world = if stage < 8 { world } else { world + 1 };
     let next_stage = if stage < 8 { stage + 1 } else { 1 };
     println!("{}-{}", next_world, next_stage);
