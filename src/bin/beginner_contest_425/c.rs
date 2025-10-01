a96efb2f611b9825ace324aa78286db96664b337
skai273 <kinoshita116@gmail.com>
2025-10-02 00:07:14 +0900

    improve beginner contest 425 c

--- src/bin/beginner_contest_425/c.rs
+++ src/bin/beginner_contest_425/c.rs
@@ -4,14 +4,14 @@ fn main() {
     let mut iter = line.split_whitespace();
     let n: usize = iter.next().unwrap().parse().unwrap();
     let q: usize = iter.next().unwrap().parse().unwrap();
-    let a_list: Vec<u32> = lines
+    let a_list: Vec<u64> = lines
         .next()
         .unwrap()
         .unwrap()
         .split_whitespace()
         .map(|a| a.parse().unwrap())
         .collect();
-    let query_list: Vec<Vec<u32>> = (0..q)
+    let query_list: Vec<Vec<u64>> = (0..q)
         .map(|_| {
             lines
                 .next()
@@ -22,7 +22,7 @@ fn main() {
                 .collect()
         })
         .collect();
-    let mut sum_list: Vec<u32> = Vec::from([0]);
+    let mut sum_list: Vec<u64> = Vec::from([0]);
     for i in 1..2 * (n + 1) {
         sum_list.push(sum_list[i - 1] + a_list[(i - 1) % n]);
     }
@@ -30,12 +30,13 @@ fn main() {
     query_list.iter().for_each(|query| match query[0] {
         1 => {
             let c = query[1] as usize;
-            head += c % n;
+            head += c;
+            head %= n;
         }
         2 => {
             let l = query[1] as usize;
             let r = query[2] as usize;
-            let sum: u32 = sum_list[head + r] - sum_list[head + l - 1];
+            let sum: u64 = sum_list[head + r] - sum_list[head + l - 1];
             println!("{}", sum);
         }
         _ => unreachable!(),
