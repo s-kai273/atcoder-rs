a64da928ca5f1e0eb5cda8ee958176187819e921
skai273 <kinoshita116@gmail.com>
2025-10-06 00:07:31 +0900

    improve beginner contest 390 b

--- src/bin/beginner_contest_390/b.rs
+++ src/bin/beginner_contest_390/b.rs
@@ -1,17 +1,16 @@
 fn main() {
     let mut lines = std::io::stdin().lines();
     let n: usize = lines.next().unwrap().unwrap().parse().unwrap();
-    let a_list: Vec<u32> = lines
+    let a_list: Vec<u64> = lines
         .next()
         .unwrap()
         .unwrap()
         .split_whitespace()
         .map(|a| a.parse().unwrap())
         .collect();
-    let r: f64 = a_list[1] as f64 / a_list[0] as f64;
     let mut answer: &str = "Yes";
-    for i in 1..n - 1 {
-        if a_list[i + 1] as f64 / a_list[i] as f64 != r {
+    for i in 0..n - 2 {
+        if a_list[i] * a_list[i + 2] != a_list[i + 1] * a_list[i + 1] {
             answer = "No";
             break;
         }
